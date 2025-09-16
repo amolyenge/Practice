eb9cba9e54125cddb1a583654496ad46dfc9b27a
Amol Yenge <amolyenge.sde2025@gmail.com>
2025-09-16 16:58:33 +0530

    Amol Created new File

---
+++ fibRecursion.java
@@ -0,0 +1,12 @@
+public class fibRecursion {
+  public static void main(String args[]) {
+	  System.out.println(fibo(8));
+  }
+  
+  public static int fibo(int n) {
+	  if(n < 2) {
+		  return n;
+	  }
+	  return fibo(n - 1) + fibo(n - 2);
+  }
+}
